175e4e8a0763411e6bdf45823828b2cbcef9fe1c
OpulentTE <la.rayl@hotmail.com>
2024-09-18 16:37:44 +0100

    updated 039_dict_operations

--- 038_dict_operations.py
+++ 038_dict_operations.py
@@ -67,7 +67,15 @@ print("Function: count_words_by_length")
 # Since there are two words of length 3, etc.
 
 def count_words_by_length(words):
-  pass
+    word_length_frequency = {}
+    for word in words:
+        word_length = len(word)
+        if word_length not in word_length_frequency:
+            word_length_frequency[word_length] = 1
+        else:
+            word_length_frequency[word_length] += 1  # Fixed indentation and compact increment
+    return word_length_frequency
+
 
 check_that_these_are_equal(
   count_words_by_length(["hat", "cat", "I", "bird"]),
