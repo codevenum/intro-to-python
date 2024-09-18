7b796f6ab1c14573cbe0f9e976b4ee53992b4870
OpulentTE <la.rayl@hotmail.com>
2024-09-18 08:16:49 +0100

    updated 019_state.py

--- 019_state.py
+++ 019_state.py
@@ -10,8 +10,15 @@ print(f"b is {b}")
 
 # @TASK: What does that output? And why? Take a guess, then
 # run the code and see.
+a = 10 # value 10 is assigned to a
+b = 20 # value 20 is assigned to b
+a = b # telling python whatever value is B should now be assigned to a
+#when we instruct python to print b, the value remains 20 has it has not changed
+20
+20
 
 # Was it what you expected?
+Yes
 
 # Try to puzzle it out, and then move on to
 # 020_state_tables.py
